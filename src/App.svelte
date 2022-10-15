579f99e1bb7ef789ae2d7151e74f0f96085f8b21
Ah3ron <ah3ron@protonmail.com>
2022-10-15 12:27:33 +0300

    added header generation

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,67 @@
 <script>
   // @ts-nocheck
+  let studentName = "Радюк Максим Александрович";
+  let groupName = "22ИТ-1";
+  let teacherName = "Вишняков Ю.М.";
+  let labNumber = "1";
+  let labName = "Целочисленные типы";
+
   function generate() {
     const doc = new docx.Document({
+      styles: {
+        paragraphStyles: [
+          {
+            id: "headingPara",
+            name: "Heading Para",
+            basedOn: "Normal",
+            quickFormat: true,
+            run: {
+              size: 28,
+            },
+          },
+        ],
+      },
       sections: [
         {
           properties: {},
           children: [
             new docx.Paragraph({
               children: [
-                new docx.TextRun("Hello World"),
                 new docx.TextRun({
-                  text: "Foo Bar",
-                  bold: true,
+                  style: "headingPara",
+                  text: "Студент " + studentName,
                 }),
+              ],
+            }),
+            new docx.Paragraph({
+              children: [
                 new docx.TextRun({
-                  text: "\tGithub is the best",
-                  bold: true,
-                  size: 16,
+                  style: "headingPara",
+                  text: "Группа " + groupName,
+                }),
+              ],
+            }),
+            new docx.Paragraph({
+              children: [
+                new docx.TextRun({
+                  style: "headingPara",
+                  text: "Преподаватель " + teacherName,
+                }),
+              ],
+            }),
+            new docx.Paragraph({
+              children: [
+                new docx.TextRun({
+                  style: "headingPara",
+                  text: "",
+                }),
+              ],
+            }),
+            new docx.Paragraph({
+              children: [
+                new docx.TextRun({
+                  style: "headingPara",
+                  text: "РАБОТА #" + labNumber + " - " + labName.toUpperCase(),
                 }),
               ],
             }),
@@ -35,8 +80,22 @@
 
 <main>
   <h2>Lab generator</h2>
+  <input bind:value={studentName} placeholder="Имя студента" />
+  <input bind:value={groupName} placeholder="Название группы" />
+  <input bind:value={teacherName} placeholder="Имя преподавателя" />
+  <input bind:value={labNumber} placeholder="Номер лабаратнорной" />
+  <input bind:value={labName} placeholder="Название лабараторной" />
   <button on:click={generate}>Generate</button>
 </main>
 
 <style>
+  main {
+    display: flex;
+    flex-direction: column;
+  }
+
+  input, button {
+    width: 250px;
+  }
+
 </style>
