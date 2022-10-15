3bcb30921ff98fa386308e27497b02b8ac01df63
Ah3ron <ah3ron@protonmail.com>
2022-10-15 11:16:24 +0300

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+  // @ts-nocheck
+  function generate() {
+    const doc = new docx.Document({
+      sections: [
+        {
+          properties: {},
+          children: [
+            new docx.Paragraph({
+              children: [
+                new docx.TextRun("Hello World"),
+                new docx.TextRun({
+                  text: "Foo Bar",
+                  bold: true,
+                }),
+                new docx.TextRun({
+                  text: "\tGithub is the best",
+                  bold: true,
+                  size: 16,
+                }),
+              ],
+            }),
+          ],
+        },
+      ],
+    });
+
+    docx.Packer.toBlob(doc).then((blob) => {
+      console.log(blob);
+      saveAs(blob, "example.docx");
+      console.log("Document created successfully");
+    });
+  }
+</script>
+
+<main>
+  <h2>Lab generator</h2>
+  <button on:click={generate}>Generate</button>
+</main>
+
+<style>
+</style>
